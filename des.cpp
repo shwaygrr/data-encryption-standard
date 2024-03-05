94eb5151c26ab9efef2f13021e0ba180b7782f18
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-03-04 21:55:16 -0500

    16 rounds enc

--- des.cpp
+++ des.cpp
@@ -20,6 +20,9 @@ int main() {
     //key
     std::bitset<64> key64(0b0001001100110100010101110111100110011011101111001101111111110001);
     
+    // std::bitset<64> plain_t64(0X1122334455667788);
+    // std::bitset<64> key64(0x752878397493CB70);
+
     ciperGen64(key64, plain_t64);
     return 0;
 }
@@ -46,7 +49,6 @@ std::vector<unsigned short int> revTable(const std::vector<unsigned short int>& 
         -   Output: Binary(28-bits) rotated left num_shift times 
 */
 void leftRotate(std::bitset<28>& bin28, unsigned int num_rots) {
-     
     /*
         -   In n<<d, last num_rots bits are 0. 
         -   To put first 3 bits of num_rots at last, do bitwise or of n<<d with n >>(INT_BITS - d)
@@ -222,64 +224,63 @@ std::bitset<32> f32(std::bitset<32> right32, std::bitset<48> subkey48) {
         unsigned short int E_bit_bit = 32-E_bit[bit];
         new_right48[bit] = right32[E_bit_bit];
     }
-
-    //XOR key and righy
+    // std::cout << new_right48 << std::endl;
+    //XOR key and right
     new_right48 = new_right48 ^= subkey48;
-
     
     // std::cout << new_right48;
     //s_box function
     const std::vector<std::vector<std::vector<int>>> S_BOXES = {
-        // S-box 1
+               // S1
         {
             {14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
             {0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
             {4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
             {15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
         },
-        // S-box 2
+        // S2
         {
             {15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
             {3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
             {0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
-            {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9}
+            {13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
         },
-        // S-box 3
+        // S3
         {
             {10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
             {13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
             {13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
             {1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
         },
-        // S-box 4
+        // S4
         {
             {7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
             {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
             {10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
             {3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
         },
-        // S-box 5
+        // S5
         {
             {2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
             {14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
             {4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
             {11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
         },
-        // S-box 6
+        // S6
         {
             {12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
             {10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
             {9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
             {4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
         },
-        // S-box 7
+        // S7
         {
             {4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
             {13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
             {1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
             {6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
         },
-        // S-box 8
+        // S8
         {
             {13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
             {1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
@@ -330,7 +331,6 @@ std::bitset<32> f32(std::bitset<32> right32, std::bitset<48> subkey48) {
     }
 
     return final32;
-
 }
 
 /*
@@ -345,15 +345,24 @@ std::bitset<64> ciperGen64 (std::bitset<64> key64, std::bitset<64> plain_t64) {
 
     //plain text permutation
     std::bitset<64> plain_t_perm64 = initPerm64(plain_t64);
+    
+    //split plain text
     std::string str_plain_t_perm64 = plain_t_perm64.to_string();
-
-    std::bitset<32> left32(str_plain_t_perm64.substr(0, 32));
-    std::bitset<32> right32(str_plain_t_perm64.substr(32));
-
-    //16 rounds of encryption on plaintext
-    auto x = left32 ^= f32(right32, subkeys48[0]);
-    std::cout << x;
+    std::bitset<32> left32(str_plain_t_perm64.substr(0, 32)); //L0
+    std::bitset<32> right32(str_plain_t_perm64.substr(32)); //R0
 
 
+    // 16 rounds of encryption on plaintext
+    for (int round = 0; round < 16; round++) {
+        std::bitset<32> temp_left32 = left32;
+        left32 = right32; //L_(i+1)
+        right32 = temp_left32 ^= f32(left32, subkeys48[round]); //R_(i+1)
+    }
+
+    std::bitset<64> post_rounds64(right32.to_string()+left32.to_string());
+    std::cout <<  post_rounds64;
+
+    
+    // std::cout << left32 << std::endl << right32;
     return key64;
 }
